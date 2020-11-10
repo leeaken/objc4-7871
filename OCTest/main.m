b16a93d9570ecf4c9f4f33650826514097a3fb14
aken <litiankun168@sina.com>
2020-11-10 16:13:49 +0800

    初始化

---
+++ OCTest/main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  OCTest
+//
+//  Created by aken on 2020/11/10.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        
+        
+        NSObject *obj = [[NSObject alloc] init];
+        NSLog(@"Hello, World!%@",obj);
+    }
+    return 0;
+}
